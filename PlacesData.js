310c93b82171b95cdfc0d2fa9c2dc8f7c57c49ad
parkherooo <168796313+parkherooo@users.noreply.github.com>
2024-06-20 21:48:01 +0900

    Create PlacesData.js

---
+++ PlacesData.js
@@ -0,0 +1,15 @@
+const placesData = [
+    { id: 1, name: '경복궁', description: '경복궁에 대한 상세 정보...', imgSrc: './public/images/경복궁.png' },
+    { id: 2, name: '광안리', description: '광안리에 대한 상세 정보...', imgSrc: './public/images/광안리.png' },
+    { id: 3, name: '대릉원', description: '대릉원에 대한 상세 정보...', imgSrc: './public/images/대릉원.png' },
+    { id: 4, name: '사계해변', description: '사계해변에 대한 상세 정보...', imgSrc: './public/images/사계해변.png' },
+    { id: 5, name: '주상절리', description: '주상절리에 대한 상세 정보...', imgSrc: './public/images/주상절리.png' },
+    { id: 6, name: '한옥마을', description: '한옥마을에 대한 상세 정보...', imgSrc: './public/images/한옥마을.png' },
+    { id: 7, name: '호미곶', description: '호미곶에 대한 상세 정보...', imgSrc: './public/images/호미곶.png' },
+    { id: 8, name: '양떼목장', description: '양떼목장에 대한 상세 정보...', imgSrc: './public/images/양떼목장.png' },
+    { id: 9, name: '에버랜드', description: '에버랜드에 대한 상세 정보...', imgSrc: './public/images/에버랜드.png' },
+    { id: 10, name: '무령왕릉', description: '무령왕릉에 대한 상세 정보...', imgSrc: './public/images/무령왕릉.png' },
+  ];
+  
+  export default placesData;
+  
